161adf377af85a9a75631af1905fafd678873f2b
gy18par <44023111+gy18par@users.noreply.github.com>
2018-12-12 13:54:15 +0000

    Update Final_Model.py

--- Final_Model.py
+++ Final_Model.py
@@ -22,6 +22,7 @@ matplotlib - a Python 2D plotting library.
 random.seed - generates a pseudorandom number which opperates on a designated
               value (agent).
 import - import function of module to make it usauble within code.
+tkinter - GUI interface
 operator - a symbol which performs an operation.
 distance - computes distance values between agents.
 pyplot - module within the matplob library allowing for basic formatting e.g.
@@ -39,12 +40,18 @@ bs4 - Beautiful Soup, v4 library for pulling data out of HTML and XML files.
 # easy (ish) to follow. 
 
 
+# Sources
+# http://www.geog.leeds.ac.uk/courses/computing/study/core-python/
+
+
 # Start
 
 import matplotlib.pyplot
 import random
 import operator
 import Agent_Framework_classes
+import csv
+import tkinter
 
 
 # Testing console 
@@ -76,6 +83,18 @@ neighbourhood =20
 random_seed = 100
 
 
+# Opening and reading in data
+f = open('Text.txt', newline='') 
+reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+for row in reader:				
+    rowlist = []
+    #rowlist.append(row)
+    for value in row:				
+        print(value) 				
+        rowlist.append(value)           
+    environment.append(rowlist) 
+
+
 # Inititlise model agents
 # Make and pull agents # beware letter cases
 for i in range(num_of_agents):
@@ -83,8 +102,8 @@ for i in range(num_of_agents):
     agents.append([random.randint(0,100),random.randint(0,100)])
     agents.append(Agent_Framework_classes.Agent(random_seed))
     print(agents)
-    
-    
+
+        
 # Plotting the agents
 matplotlib.pyplot.xlim(0, 99)
 matplotlib.pyplot.ylim(0, 99)
@@ -118,10 +137,13 @@ for j in range(num_of_iterations):
             agents[i].eat()
             agents[i].share_with_neighbours(neighbourhood)
 
-
+        
 # Testing agent distance calculation
 def distance_between(self, agent):
     return (((self.x - agent.x)**2) + ((self.y - agent.y)**2))**0.5
+
+
+# print (agents)
 
 
 # Calcuclate environment eaten
@@ -129,5 +151,37 @@ total = 0
 for a in agents:
     total +=()
     print(total)
+       
+
+# Save agent data to csv file
+f2 = open('dataout.csv', 'w', newline='') 
+writer = csv.writer(f2, delimiter=',')
+for i in agents:		
+	writer.writerow(row)		
+f2.close()
+
+
+# Commence animation 
+def run():
+    animation = matplotlib.animation.FuncAnimation(fig, update, interval=1, 
+                                                   repeat=False, frames=num_of_iterations)
+    canvas.show() 
+    
+
+# Modify GUI
+root = tkinter.Tk() 
+root.wm_title("Model")
+canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=root)
+canvas._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1) 
+
+menu_bar = tkinter.Menu(root)
+root.config(menu=menu_bar)
+model_menu = tkinter.Menu(menu_bar)
+menu_bar.add_cascade(label="Model", menu=model_menu)
+model_menu.add_command(label="Run model", command=run) 
+
+
+# Display window
+tkinter.mainloop() 
 
 # End
